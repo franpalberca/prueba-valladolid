1f72ae65a8ca66e2f357206dc5769684bc4cdaae
Fran Pérez Alberca <fran.perez.alberca@gmail.com>
2024-01-05 12:34:40 +0100

    feat(statistics)fetch statistics from server

--- client/src/chart/LinesChart.tsx
+++ client/src/chart/LinesChart.tsx
@@ -1,63 +1,32 @@
-import { Line } from 'react-chartjs-2'
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler
-} from 'chart.js'
+import {Line} from 'react-chartjs-2';
+import {Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler} from 'chart.js';
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler
-);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler);
 
-const dias = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const fatiga = [4, 5, 3, 4, 2, 4, 1, 1]
-const mydata = {
-    labels: dias,
-    datasets: [
-        {
-            label: 'Fatiga',
-            data: fatiga,
-            tension: 0.5,
-            fill: true,
-            borderColor: 'rgb(255, 99, 132',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            pointRadius: 5,
-            pointBorderColor: 'rgba(255, 99, 132)',
-            pointBackgroundColor: 'rgba(255,99, 132)'
-        }
-    ]
+interface LinesChartProps {
+	chartData: any;
 }
-const myoptions = {
-    scales: {
-        y: {
-            min: 0
+
+const LinesChart: React.FC<LinesChartProps> = ({chartData}) => {
+
+    const myoptions = {
+        scales: {
+            y: {
+                min: 0,
+            },
+            x: {
+                ticks: {
+                    color: 'green',
+                },
+            },
         },
-        x: {
-            ticks: {
-                color: 'blue'
-            }
-        }
-    },
-    plugins: {
-        legend: {
-            display: false
-        }
-    }
-}
+        plugins: {
+            legend: {
+                display: true,
+            },
+        },
+    };
+	return <Line data={chartData} options={myoptions} />;
+};
 
-export default function LinesChart() {
-    return <Line data={mydata} options={myoptions} />
-}
+export default LinesChart;
--- client/src/pages/private/StatisticsPlayerPage.tsx
+++ client/src/pages/private/StatisticsPlayerPage.tsx
@@ -1,9 +1,10 @@
 import {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
-import {Image} from 'react-bootstrap';
+import {useNavigate, useParams} from 'react-router-dom';
+import {Button, Image} from 'react-bootstrap';
 import styled from 'styled-components';
-import LinesChart from '../../chart/LinesChart.tsx'
-
+import LinesChart from '../../chart/LinesChart.tsx';
+import { format } from 'date-fns';
+import { STATISTICS } from '../../config/routes/paths.ts';
 
 interface FootballerInfo {
 	message: string;
@@ -15,14 +16,101 @@ interface FootballerInfo {
 		updatedAt: string;
 	};
 }
+interface Answer {
+    answerId: string;
+    answerOne: number;
+    answerTwo: number;
+    answerThree: number;
+    answerFour: number;
+    answerFive: number;
+    createdAt: string;
+}
 
 const url = import.meta.env.VITE_API_URL;
 const StatisticsPlayerPage = () => {
 	const {footballerId} = useParams();
 	const [playerInfo, setPlayerInfo] = useState<FootballerInfo | null>(null);
-	console.log(playerInfo);
+	const [chartData, setChartData] = useState<any>({labels: [], datasets: []});
+	const navigate = useNavigate()
 
 	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(`http://localhost:8080/api/footballer/answer/2/${footballerId}`);
+				const data = await response.json();
+
+				if (!data.answers || !Array.isArray(data.answers)) {
+					console.error('Invalid data format:', data);
+					return;
+				}
+
+				setChartData({
+					labels: data && data?.answers?.map((answer: Answer) => format(new Date(answer.createdAt), 'dd-MM-yyyy')) || [],
+					datasets: [
+						{
+							label: 'Fatiga',
+							data: (data && data?.answers?.map((answer: Answer) => answer.answerOne)) || [],
+							tension: 0.5,
+							fill: true,
+							borderColor: 'rgb(255, 99, 132)',
+							backgroundColor: 'rgba(255, 99, 132, 0.5)',
+							pointRadius: 5,
+							pointBorderColor: 'rgba(255, 99, 132)',
+							pointBackgroundColor: 'rgba(255, 99, 132)',
+						},
+						{
+							label: 'Sueño',
+							data: data && data?.answers?.map((answer: Answer) => answer.answerTwo),
+							tension: 0.5,
+							fill: true,
+							borderColor: 'rgb(255, 99, 132)',
+							backgroundColor: 'rgba(255, 99, 132, 0.5)',
+							pointRadius: 5,
+							pointBorderColor: 'rgba(255, 99, 132)',
+							pointBackgroundColor: 'rgba(255, 99, 132)',
+						},
+						{
+							label: 'Dolor Muscular',
+							data: data && data?.answers?.map((answer: Answer) => answer.answerThree),
+							tension: 0.5,
+							fill: true,
+							borderColor: 'rgb(255, 99, 132)',
+							backgroundColor: 'rgba(255, 99, 132, 0.5)',
+							pointRadius: 5,
+							pointBorderColor: 'rgba(255, 99, 132)',
+							pointBackgroundColor: 'rgba(255,99, 132)',
+						},
+						{
+							label: 'Stress',
+							data: data && data?.answers?.map((answer: Answer) => answer.answerFour),
+							tension: 0.5,
+							fill: true,
+							borderColor: 'rgb(255, 99, 132)',
+							backgroundColor: 'rgba(255, 99, 132, 0.5)',
+							pointRadius: 5,
+							pointBorderColor: 'rgba(255, 99, 132)',
+							pointBackgroundColor: 'rgba(255,99, 132)',
+						},
+						{
+							label: 'Estado anímico',
+							data: data && data?.answers?.map((answer: Answer) => answer.answerFive),
+							tension: 0.5,
+							fill: true,
+							borderColor: 'rgb(255, 99, 132)',
+							backgroundColor: 'rgba(255, 99, 132, 0.5)',
+							pointRadius: 5,
+							pointBorderColor: 'rgba(255, 99, 132)',
+							pointBackgroundColor: 'rgba(255,99, 132)',
+						},
+					],
+				});
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		};
+
+		fetchData();
+
 		fetch(`${url}api/footballer/${footballerId}`)
 			.then((response) => response.json())
 			.then((data: FootballerInfo) => {
@@ -35,21 +123,25 @@ const StatisticsPlayerPage = () => {
 		return <div>Cargando...</div>;
 	}
 
+	const handleBack = () => {
+        navigate(STATISTICS)
+    }
+
 	return (
 		<StatisticsStyles>
 			<h1 className="title">Estadísticas del Jugador</h1>
-            <div className='d-flex'>
-                <div>
-			<Image id={footballerId} src={playerInfo.Footballer.footballerPicture} alt={playerInfo.Footballer.footballerName} className="playerPicture" />
-			<h2 className="subtitle">Nombre: {playerInfo.Footballer.footballerName}</h2>
-            </div>
-            <div className=''>
-            <p className='m-2'><b>Ejemplo #1: </b>Gráfico de líneas básico</p>
-            <div className='bg-light mx-auto px-2 border border-2 border-primary' style={{width: '450px', height: '230px'}}>
-            <LinesChart />
-            </div>
-            </div>
-            </div>
+			<div className="d-flex all_info">
+				<div>
+					<Image id={footballerId} src={playerInfo.Footballer.footballerPicture} alt={playerInfo.Footballer.footballerName} className="playerPicture" />
+					<h2 className="subtitle">Nombre: {playerInfo.Footballer.footballerName}</h2>
+				</div>
+				<div className="grafic">
+					<div className="bg-light mx-auto px-2 border border-2 border-primary" style={{width: '600px', height: '310px'}}>
+						<LinesChart chartData={chartData} />
+					</div>
+				</div>
+			</div>
+			<Button className='back_button' onClick={handleBack}>Ir atrás</Button>
 		</StatisticsStyles>
 	);
 };
@@ -59,6 +151,9 @@ export default StatisticsPlayerPage;
 const StatisticsStyles = styled.div`
 	background-color: #921b88;
 	color: white;
+	& .all_info {
+		margin-left: 15vh;
+	}
 	& .title {
 		display: flex;
 		align-items: center;
@@ -70,5 +165,16 @@ const StatisticsStyles = styled.div`
 	}
 	& .subtitle {
 		margin: 15px;
+	}
+	& .grafic{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		margin-left: 8vh;
+	}
+	& .back_button {
+		margin-left: 100vh;
+		height: 10vh
 	}
 `;
