820c7a1c4d65be5529e869c664eb0baea6311943
Fran Pérez Alberca <fran.perez.alberca@gmail.com>
2024-01-05 10:21:07 +0100

    feat(chart)add chart for players with fake data

--- client/src/pages/private/StatisticsPlayerPage.tsx
+++ client/src/pages/private/StatisticsPlayerPage.tsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import {Image} from 'react-bootstrap';
 import styled from 'styled-components';
-import LinesChart from '../../chart/LinesChart'
+import LinesChart from '../../chart/LinesChart.tsx'
 
 
 interface FootballerInfo {
